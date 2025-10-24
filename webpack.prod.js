8b43ed32bb864026f342154aea1f8e095a3ded15
ITCHYNS <izharfadillah19@gmail.com>
2025-10-24 23:30:34 +0700

    gh edit

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,4 @@
+const path = require('path'); 
 const common = require('./webpack.common.js');
 const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
@@ -5,6 +6,11 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  output: {                                
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
   module: {
     rules: [
       {
